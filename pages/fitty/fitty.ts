54795ad299a434a262820af9dbc391df421dd176
KaiserStefan <31877980+KaiserStefan@users.noreply.github.com>
2018-11-24 16:26:04 +0100

    two

--- pages/fitty/fitty.ts
+++ pages/fitty/fitty.ts
@@ -26,6 +26,16 @@ export class FittyPage {
   private rightpupil;
   private nose;
 
+  private bauch;
+
+  private mouth;
+  private lip;
+
+  private pawleft;
+  private pawright;
+  private footleft;
+  private footright;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
     ionViewDidLoad() {
@@ -35,6 +45,9 @@ export class FittyPage {
 
     ionViewDidEnter() {
       this.stage = new createjs.Stage("demoCanvas");
+      this.parentcontainer = new createjs.Container();
+      this.body = new createjs.Container();
+      this.head = new createjs.Container();
 
       this.schedel = new createjs.Shape();
       this.schedel.graphics
@@ -68,13 +81,14 @@ export class FittyPage {
       this.rightear = new createjs.Shape();
       this.rightear.graphics.beginStroke("black").beginFill("white")
         .setStrokeStyle(20).moveTo(1520,640)
-        .quadraticCurveTo(1440,570,1550,430)
-        .quadraticCurveTo(1690,320,1760,500)
-        .quadraticCurveTo(1780,600,1700,700)
+        .quadraticCurveTo(1440,570,1560,430)
+        .quadraticCurveTo(1690,330,1735,500)
+        .quadraticCurveTo(1750,600,1680,700)
         //.bezierCurveTo(100,100,200,200,500,500)
         //.arc(0, 0,70,0, ((Math.PI*2)/360)*170, 1);
         .endStroke()
       ;
+      /*
       this.rightearouter = new createjs.Shape();
       this.rightearouter.graphics.beginStroke("black").beginFill(this.colorbrown)
         .setStrokeStyle(20).moveTo(1450,640)
@@ -83,6 +97,14 @@ export class FittyPage {
         .quadraticCurveTo(1840,620,1750,750)
         //.bezierCurveTo(100,100,200,200,500,500)
         //.arc(0, 0,70,0, ((Math.PI*2)/360)*170, 1);
+        .endStroke()
+      ;
+      */
+
+      this.rightearouter = new createjs.Shape();
+      this.rightearouter.graphics.beginStroke("black").beginFill(this.colorbrown)
+        .setStrokeStyle(20).moveTo(1400,640)
+        .bezierCurveTo(1420,180,2050,220,1750,750)
         .endStroke()
       ;
 
@@ -105,7 +127,7 @@ export class FittyPage {
 
       this.rightpupil = new createjs.Shape();
       this.rightpupil.graphics.beginFill("black").drawEllipse(0,0,110,180);
-      this.rightpupil.x = 1280;
+      this.rightpupil.x = 1290;
       this.rightpupil.y = 850;
       this.rightpupil.rotation = 18;
 
@@ -114,15 +136,68 @@ export class FittyPage {
       this.nose.x =1050;
       this.nose.y =1200;
 
-      this.head = new createjs.Container();
+      this.mouth = new createjs.Shape();
+      this.lip = new createjs.Shape();
+
+      this.mouth.graphics.beginStroke("black")
+        .setStrokeStyle(20)
+        .beginFill("#701228")
+        .moveTo(980,1320)
+        .bezierCurveTo(1000,1390,1120,1410,1150,1350)
+        .bezierCurveTo(1200,1580,870,1560,980,1320)
+        .endFill()
+        .endStroke()
+      ;
+      this.lip.graphics.beginStroke("black").beginFill("#DD4368")
+        .setStrokeStyle(20).moveTo(960,1430)
+        .bezierCurveTo(1010,1420,1050,1425,1138,1460)
+        .bezierCurveTo(1090,1540,990,1530,960,1430)
+        .endStroke()
+      ;
+
+      this.bauch = new createjs.Shape();
+      this.bauch.graphics
+        .beginStroke("black").setStrokeStyle(20).beginFill(this.colorbrown)
+          .drawRoundRectComplex(720,1400,850,900,0,0,20,20)
+        /*
+        .lineTo(780,2300)
+        .arcTo(770,2310,790,2300,10)
+        */;
+      this.bauch.rotation = 2;
+
+      this.pawleft = new createjs.Shape();
+      this.pawright = new createjs.Shape();
+      this.footleft = new createjs.Shape();
+      this.footright = new createjs.Shape();
+      this.pawleft.graphics.beginStroke("black").beginFill(this.colorbrown)
+        .setStrokeStyle(20).moveTo(550,1450)
+        .bezierCurveTo(250,1800,600,2000,800,1500)
+        .endStroke()
+      ;
+      this.pawright.graphics.beginStroke("black").beginFill(this.colorbrown)
+        .setStrokeStyle(20).moveTo(1400,1450)
+        .bezierCurveTo(1150,2000,1750,2200,1550,1500)
+        .endStroke()
+      ;
+      this.footleft.graphics.beginStroke("black").beginFill(this.colorbrown)
+        .setStrokeStyle(20).moveTo(650,2200)
+        .bezierCurveTo(400,2400,700,2600,800,2300)
+        .endStroke()
+      ;
+      this.footright.graphics.beginStroke("black").beginFill(this.colorbrown)
+        .setStrokeStyle(20).moveTo(1300,2200)
+        .bezierCurveTo(1250,2600,1750,2600,1490,2250)
+        .endStroke()
+      ;
+
+
+
+      this.body.addChild(this.bauch);
       this.head.addChild(this.leftearouter, this.rightearouter, this.leftear, this.rightear, this.schedel, this.lefteye,
-        this.righteye, this.leftpupil, this.rightpupil, this.nose);
-
-      this.body = new createjs.Container();
+        this.righteye, this.leftpupil, this.rightpupil, this.nose, this.mouth, this.lip);
 
 
-      this.parentcontainer = new createjs.Container();
-      this.parentcontainer.addChild(this.body, this.head);
+      this.parentcontainer.addChild(this.footleft, this.footright, this.body, this.pawleft, this.pawright, this.head);
 
       this.stage.addChild(this.parentcontainer);
 
